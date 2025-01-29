d6ea87d448bad4f6fca770f6cb96d10cb6f6bf57
wyattcolburn <wyattdcolburn@gmail.com>
2025-01-28 22:02:36 -0800

    test dependecies loads empty world

--- src/my_robot_bringup/launch/test_backup.launch.py
+++ src/my_robot_bringup/launch/test_backup.launch.py
@@ -1,28 +1,10 @@
-# Copyright 2023 Clearpath Robotics, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# @author Roni Kreinin (rkreinin@clearpathrobotics.com)
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-#from launch.actions import ExecuteProcess
 
+# Declare arguments
 ARGUMENTS = [
     DeclareLaunchArgument('namespace', default_value='',
                           description='Robot namespace'),
@@ -44,13 +26,13 @@ def generate_launch_description():
     # Directories
     pkg_turtlebot4_ignition_bringup = get_package_share_directory(
         'turtlebot4_ignition_bringup')
-
+    pkg_my_robot_bringup = get_package_share_directory(
+        'my_robot_bringup')
     # Paths
     ignition_launch = PathJoinSubstitution(
         [pkg_turtlebot4_ignition_bringup, 'launch', 'ignition.launch.py'])
     robot_spawn_launch = PathJoinSubstitution(
         [pkg_turtlebot4_ignition_bringup, 'launch', 'turtlebot4_spawn.launch.py'])
-
     ignition = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([ignition_launch]),
         launch_arguments=[
@@ -58,6 +40,7 @@ def generate_launch_description():
         ]
     )
 
+    
     robot_spawn = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([robot_spawn_launch]),
         launch_arguments=[
@@ -68,15 +51,17 @@ def generate_launch_description():
             ('z', LaunchConfiguration('z')),
             ('yaw', LaunchConfiguration('yaw'))]
     )
-    # Command to execute the Python script
-    #print("Starting the bag")
-    #record_bag_process = ExecuteProcess(
-    #    cmd=["python3", "/home/wyattcolburn/utils/bag_lidar.py", LaunchConfiguration("output_dir"), LaunchConfiguration("topics")],
-    #    output="screen"
-    #)
+    
+    bag_record = ExecuteProcess(
+        cmd=['ros2', 'bag', 'record', '/scan', '/tf', '/tf_static', '/odom'],
+        output='screen'
+        )
+
+
     # Create launch description and add actions
     ld = LaunchDescription(ARGUMENTS)
     ld.add_action(ignition)
     ld.add_action(robot_spawn)
+    ld.add_action(bag_record)
     return ld
 
