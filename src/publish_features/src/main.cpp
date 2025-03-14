7017577477aeca69a085a6e565af1f2616fc20cd
wyattcolburn <wyattdcolburn@gmail.com>
2025-03-14 13:53:53 -0700

    documented publish node

--- src/publish_features/src/main.cpp
+++ src/publish_features/src/main.cpp
@@ -1,3 +1,19 @@
+/*
+ * The purpose of this node is to publish the feature data to the neural net plug in
+ *
+ * Packet Structure: Float64 Multi Array message size, because odom data is doubled
+ *					 Lidar data is sized up from floats to doubles so that data can
+ *					 all be published using one array/message. The array is static for 
+ *					 performance reasons. 
+ *
+ *Packet Declaration:
+ * <odom_x, odom_y, odom_v, odom_w, lidar_ranges [1080 values]
+ *
+ *Improvements: Need to add the local values, not sure how we get those values, would like
+ *			    to verify the syncronzation is working
+*/
+
+
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
@@ -30,25 +46,23 @@ public:
   }
 
 private:
-  // Private because only class is going to access it, fixed in size
-  //
   // Packet definition 
   // doubles < odom_x, odom_y, odom_v, odom_w, need local_goals_x, local_goal_y, lidar data >
+  
   static constexpr size_t ODOM_FIELD_COUNT = 4;
   static constexpr size_t LIDAR_COUNT = 1080;
   double packetOut[ODOM_FIELD_COUNT + LIDAR_COUNT];
 
 
   // Callback function for the /odom subscriber.
-  
   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
   {
       packetOut[0] = msg->pose.pose.position.x,
       packetOut[1] =msg->pose.pose.position.y,
 	  packetOut[2] = msg->twist.twist.linear.x,
  	  packetOut[3] = msg->twist.twist.angular.z;
-	  std::cout << "Type of odom:     " << typeid(msg->pose.pose.position.x).name() << std::endl;
-    RCLCPP_INFO(
+    
+	  RCLCPP_INFO(
       this->get_logger(),
       "Received /odom: position [x: %.2f, y: %.2f, odom_v : %.2f, odom_w : %.2f, odom_w], ",
 	  packetOut[0],
@@ -58,10 +72,12 @@ private:
   }
 
   void lidar_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
+	  //printing out how many ranges there are to verify sim working 
 	  size_t num_ranges = msg->ranges.size();
 	
 	  std::cout << "Type of ranges:     " << typeid(msg->ranges[0]).name() << std::endl;
 	  size_t offset = ODOM_FIELD_COUNT;
+	  //upscaling floats to doubles
 	  std::transform(msg->ranges.begin(), msg->ranges.end(), packetOut + offset,
 			[](float value) -> double { return static_cast<double>(value); });
 	  RCLCPP_INFO(
