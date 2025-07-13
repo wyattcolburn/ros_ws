12eaafd3eb3328259656d1029ba058939d921694
WyattColburn <wyattdcolburn@gmail.com>
2025-07-12 17:07:49 -0700

    ADD: Barn test

---
+++ src/my_robot_bringup/launch/barn_test.launch.py
@@ -0,0 +1,73 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch.actions import TimerAction
+
+# Declare arguments
+ARGUMENTS = [
+    DeclareLaunchArgument('namespace', default_value='',
+                          description='Robot namespace'),
+    DeclareLaunchArgument('rviz', default_value='false',
+                          choices=['true', 'false'], description='Start rviz.'),
+    DeclareLaunchArgument('world', default_value='maze',
+                          description='Ignition World'), DeclareLaunchArgument('model', default_value='standard',
+                          choices=['standard', 'lite'],
+                          description='Turtlebot4 Model'),
+]
+
+for pose_element in ['x', 'y', 'z', 'yaw']:
+    ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value='0.0',
+                     description=f'{pose_element} component of the robot pose.'))
+
+
+def generate_launch_description():
+    # Directories
+    pkg_turtlebot4_ignition_bringup = get_package_share_directory(
+        'turtlebot4_ignition_bringup')
+    pkg_my_robot_bringup = get_package_share_directory(
+        'my_robot_bringup')
+    # Paths
+    ignition_launch = PathJoinSubstitution(
+        [pkg_turtlebot4_ignition_bringup, 'launch', 'ignition.launch.py'])
+    robot_spawn_launch = PathJoinSubstitution(
+        [pkg_turtlebot4_ignition_bringup, 'launch', 'turtlebot4_spawn.launch.py'])
+    ignition = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([ignition_launch]),
+        launch_arguments=[
+            ('world', LaunchConfiguration('world'))
+        ]
+    )
+
+    
+    robot_spawn = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([robot_spawn_launch]),
+        launch_arguments=[
+            ('namespace', LaunchConfiguration('namespace')),
+            ('rviz', LaunchConfiguration('rviz')),
+            ('x', LaunchConfiguration('x')),
+            ('y', LaunchConfiguration('y')),
+            ('z', LaunchConfiguration('z')),
+            ('yaw', LaunchConfiguration('yaw'))]
+    )
+
+    undock_command = ExecuteProcess(
+        cmd=['ros2', 'action', 'send_goal', '/undock', 'irobot_create_msgs/action/Undock', '{}'],
+        output='screen'
+    )
+    undock_with_delay = TimerAction(
+        period=25.0,  # Delay in seconds
+        actions=[undock_command]
+
+    # Create launch description and add actions
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(ignition)
+    ld.add_action(robot_spawn)
+    # ld.add_action(init_command)
+    ld.add_action(undock_with_delay)
+    #ld.add_action(goal_command)
+    return ld
+
+
